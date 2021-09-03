72081bc802dd4ce7bff6557e68db73b3511f0979
Matheus Geiser <matheus.lgeiser@ambevtech.com.br>
2021-09-03 16:51:50 -0300

    criando arquivo da login e arrumando rota inicial do app

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -4,6 +4,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import '../core/domain/usecases/authentitcate_user_use_case.dart';
 import '../core/external/datasource/api_datasource.dart';
 import '../core/infra/repositories/auth_repository_impl.dart';
+import 'core/presenter/login_screen.dart';
 
 
 class AppModule extends Module {
@@ -17,5 +18,6 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
+    ChildRoute('/', child: (_, args) => LoginScreen()),
   ];
 }
