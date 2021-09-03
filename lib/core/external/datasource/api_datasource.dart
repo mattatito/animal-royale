059d22513051b58576281bf5a6937ba5f954ab25
Matheus Geiser <matheus.lgeiser@ambevtech.com.br>
2021-09-03 16:06:25 -0300

    finalizando datasource

---
+++ lib/core/external/datasource/api_datasource.dart
@@ -0,0 +1,24 @@
+
+import 'package:animal_royale/core/infra/datasources/authenticate_user.dart';
+import 'package:animal_royale/core/infra/models/result_authentication_model.dart';
+import 'package:dio/dio.dart';
+
+class ApiDatasource implements AuthenticateUserDatasource{
+  final Dio dio;
+
+  ApiDatasource(this.dio);
+
+  @override
+  Future<ResultAuthenticationModel> authenticate(String userEmail, String userPassword) async {
+    final response = await dio.get("http://191.232.488");
+
+    if(response.statusCode != 200){
+      throw Exception("Error");
+    }
+
+    return ResultAuthenticationModel.fromJson(response.data as String);
+
+
+  }
+
+}
