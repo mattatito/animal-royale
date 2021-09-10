0d0a6fb4beb27819f013c19fe9f4c1c5631d5fa6
matheus.geiser <matheusgeiser@gmail.com>
2021-09-10 10:24:23 -0300

    Criando item da lista

--- lib/modules/home/presentation/home_screen.dart
+++ lib/modules/home/presentation/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:animal_royale/modules/home/presentation/widgets/app_bar/app_bar_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/ItemListTile.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -9,22 +11,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var _rating = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(
-        title: 'Bem vindo!',
-        height: 112,
-        subtitle: 'Confira os animaizinhos do joguinho ',
-      ),
-      body: Container(
-        child: Slider(
-          value: _rating,
-          onChangeEnd: (v) {},
-          onChanged: (newValue) => setState(() => _rating = newValue),
-          min: 0,
-          max: 100,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBarWidget(
+          title: 'Bem vindo!',
+          height: 112,
+          subtitle: 'Confira os animaizinhos do joguinho ',
+        ),
+        body: Column(
+          children: [
+            ItemListTile(
+              title: 'Fox',
+              imageUrl:
+                  'https://static.wikia.nocookie.net/animalroyale_gamepedia_en/images/0/06/Char-fox.png/revision/latest/scale-to-width-down/152?cb=20201124165055',
+              onTap: (){},
+            ),
+          ],
         ),
       ),
     );
