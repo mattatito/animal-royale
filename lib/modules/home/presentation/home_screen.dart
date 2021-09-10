9abc7fdf403f48502cab082af6dd00b067a6e631
matheus.geiser <matheusgeiser@gmail.com>
2021-09-10 18:33:52 -0300

    adicionando responsividade nos cards

--- lib/modules/home/presentation/home_screen.dart
+++ lib/modules/home/presentation/home_screen.dart
@@ -13,23 +13,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBarWidget(
           title: 'Bem vindo!',
-          height: 112,
-          subtitle: 'Confira os animaizinhos do joguinho ',
+          height: size.height * 0.15,
+          subtitle: 'Confira os personagens de Super Animal Royale! ',
         ),
-        body: Column(
-          children: [
-            ItemListTile(
-              title: 'Fox',
-              imageUrl:
-                  'https://static.wikia.nocookie.net/animalroyale_gamepedia_en/images/0/06/Char-fox.png/revision/latest/scale-to-width-down/152?cb=20201124165055',
-              onTap: (){},
-              detailsColor: Color(0xff595959),
-            ),
-          ],
+        body: Container(
+          margin: EdgeInsets.only(top: 10),
+          child: Column(
+            children: [
+              ItemListTile(
+                title: 'Fox',
+                imageUrl:
+                    'https://static.wikia.nocookie.net/animalroyale_gamepedia_en/images/0/06/Char-fox.png/revision/latest/scale-to-width-down/152?cb=20201124165055',
+                onTap: (){},
+                detailsColor: Color(0xff595959),
+              ),
+              ItemListTile(
+                title: 'Bear',
+                imageUrl:
+                    'https://static.wikia.nocookie.net/animalroyale_gamepedia_en/images/6/69/Char-bear.png/revision/latest/scale-to-width-down/152?cb=20201124171657',
+                onTap: (){},
+                detailsColor: Color(0xff595959),
+              ),
+              ItemListTile(
+                title: 'SkullCat',
+                imageUrl:
+                    'https://static.wikia.nocookie.net/animalroyale_gamepedia_en/images/5/56/Char-skullcat.png/revision/latest/scale-to-width-down/152?cb=20201123045143',
+                onTap: (){},
+                detailsColor: Color(0xff595959),
+              ),
+              ItemListTile(
+                title: 'Tiger',
+                imageUrl:
+                    'https://static.wikia.nocookie.net/animalroyale_gamepedia_en/images/1/11/Char-tiger.png/revision/latest/scale-to-width-down/152?cb=20201122175457',
+                onTap: (){},
+                detailsColor: Color(0xff595959),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/modules/home/presentation/widgets/ItemListTile.dart
+++ lib/modules/home/presentation/widgets/ItemListTile.dart
@@ -6,33 +6,56 @@ class ItemListTile extends StatelessWidget {
   final VoidCallback onTap;
   final Color detailsColor;
 
-  ItemListTile({Key? key, required this.title, required this.imageUrl, required this.onTap, required this.detailsColor}) : super(key: key);
+  ItemListTile(
+      {Key? key,
+      required this.title,
+      required this.imageUrl,
+      required this.onTap,
+      required this.detailsColor})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return InkWell(
       splashColor: Colors.black,
       onTap: onTap,
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         decoration: BoxDecoration(
-          border: Border.fromBorderSide(BorderSide(color: detailsColor, width: 1)),
+            border: Border.fromBorderSide(
+                BorderSide(color: detailsColor, width: 1)),
             color: Colors.grey.withOpacity(0.3),
             borderRadius: BorderRadius.circular(10)),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(image: NetworkImage(imageUrl), fit: BoxFit.cover),
-                  color: detailsColor,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10))
-              ),
-              width: 70,
-              height: 100,
+            Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: detailsColor,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10))),
+                  width: size.width * 0.24,
+                  height: size.height * 0.13,
+                ),
+                Container(
+                  width: size.width * 0.26,
+                  height: size.height * 0.13,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(imageUrl), fit: BoxFit.cover),
+                  ),
+                )
+              ],
             ),
             Text(title, style: TextStyle(fontSize: 20)),
-            Container(height: 1, color: detailsColor,),
+            Container(
+              height: 1,
+              color: detailsColor,
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Icon(Icons.arrow_forward_ios),
