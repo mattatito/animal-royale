edbaa66b3ea8947b9d58f12b483503439527c6c3
matheus.geiser <matheusgeiser@gmail.com>
2021-09-14 14:49:02 -0300

    Adicionando localização

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
@@ -6,6 +7,15 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animal Royale',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''), // English, no country code
+        Locale('pt', ''), // Portuguese, no country code
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
